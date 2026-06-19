099916c38cff1389c73078fdef2baae105570800
Elefinnt <hackshawf5@gmail.com>
2026-06-19 21:58:40 +1200

    chore: update packages/shared/src/battle/index.ts

---
+++ packages/shared/src/battle/index.ts
@@ -0,0 +1,9 @@
+/** Barrel for the battle engine. Import from "@monstro/shared". */
+export * from "./types.js";
+export * from "./rng.js";
+export * from "./data/index.js";
+export * from "./damage.js";
+export * from "./factory.js";
+export * from "./engine.js";
+export * from "./ai.js";
+export * from "./encounters.js";
